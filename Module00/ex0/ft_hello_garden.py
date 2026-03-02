71999967f98b8ce3a980dfcddba50e5b8ba97d5d
Yuanxin Wang <yuawang@2-B-2.42heilbronn.de>
2026-03-02 12:28:29 +0100

    Last week

---
+++ Module00/ex0/ft_hello_garden.py
@@ -0,0 +1,4 @@
+def ft_hello_garden():
+    hello = "Hello, Garden Community!"
+    print(hello)
+    print("Hello, Garden Community!")
---
+++ Module00/ex5/ft_count_harvest_iterative.py
@@ -0,0 +1,2 @@
+def ft_count_harvest_literative():
+    days = int(input("Days until harvest: "))
