1f7f393e9a6e07170bb0d7f68f0ae997f2c0a588
Yuanxin <yuanxin.wang9@gmail.com>
2026-03-04 13:00:54 +0100

    ex4

---
+++ Module01/ex4/ft_garden_security.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+class SecurePlant:
+    # first set to zero to secure the data initialization in case of error
+    def __init__(self, name: str, height: int, age: int) -> None:
+        self._name = name
+        print(f"Plant created: {self._name}")
+        self._height = 0
+        self._age = 0
+        self.set_height(height)
+        self.set_age(age)
+
+    def set_height(self, height: int) -> None:
+        if height < 0:
+            print(
+                f"\nInvalid operation attempted: height {height}cm [REJECTED]"
+            )
+            print("Security: Negative height rejected")
+        else:
+            self._height = height
+            print(f"Height updated: {height}cm [OK]")
+
+    def set_age(self, age: int) -> None:
+        if age < 0:
+            print(f"\nInvalid operation attempted: age {age} days [REJECTED]")
+            print("Security: Negative age rejected")
+        else:
+            self._age = age
+            print(f"Age updated: {age} days [OK]")
+
+    def get_height(self) -> int:
+        return self._height
+
+    def get_age(self) -> int:
+        return self._age
+
+    def get_info(self) -> str:
+        return (
+            f"\nCurrent plant: {self._name} "
+            f"({self._height}cm, {self._age} days)"
+        )
+
+
+def main() -> None:
+    print("=== Garden Secutrity System ===")
+    rose = SecurePlant("Rose", 25, 30)
+    rose.set_height(-5)
+    print(rose.get_info())
+
+
+if __name__ == "__main__":
+    main()
